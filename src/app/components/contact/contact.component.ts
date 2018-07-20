a96ded8c9808c77b826c5a227b70b1b5f4ba3b14
alvics <alvics71@hotmail.com>
2018-07-20 14:11:02 +1000

    add contact-form componets

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
+  contactForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+      this.contactForm = fb.group({
+          contactFormName: ['', Validators.required],
+          contactFormEmail: ['', [Validators.required, Validators.email]],
+          contactFormSubject: ['', Validators.required],
+          contactFormMessage: ['', Validators.required]
+      });
   }
-
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBXyTX78wmy1gqQjsLb4UIYCniFeA5aerw",
+    authDomain: "purebody-coffescrub.firebaseapp.com",
+    databaseURL: "https://purebody-coffescrub.firebaseio.com",
+    projectId: "purebody-coffescrub",
+    storageBucket: "",
+    messagingSenderId: "468810247455"
+
+  }
 };
 
 /*
